7e897b41d7af1cc88e6ae74ee64d68d2595c6cf5
Slava <migrant405@gmail.com>
2023-02-08 14:34:45 +0400

    sem_2_add_description

--- Seminar_2/sem_2_1.java
+++ Seminar_2/sem_2_1.java
@@ -7,6 +7,13 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
+
+
+//Напишите программу, которая принимает с консоли число в формате byte и записывает его в файл ”result.txt”.
+//Требуется перехватить все возможные ошибки и вывести их в логгер.
+//
+//После написания, попробуйте подать на вход числа 100 и 200 и проследите разницу в результате
+
 public class sem_2_1 
 {
    public static void main(String[] args) 
